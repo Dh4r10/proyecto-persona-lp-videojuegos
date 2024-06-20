c5c6e0a4d497ba430841195af39f489b02ea41c7
Dh4rio <marzchelo1@gmail.com>
2024-06-20 03:31:32 -0500

    modificaicones en mi index html

--- src/main/java/proyecto/personal/dhario/videojuegos/Config/SecurityConfig.java
+++ src/main/java/proyecto/personal/dhario/videojuegos/Config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(request -> request.requestMatchers("/css/*", "/img/*").permitAll()
+        http.authorizeHttpRequests(request -> request.requestMatchers("/","/css/*", "/img/*").permitAll()
                     .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login").permitAll())
                 .logout(LogoutConfigurer::permitAll);
